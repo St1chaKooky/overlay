cbae417466e422e37e6dc9dc21ebca42a5a3e544
wov1a <vova.stoan2006@gmail.com>
2024-06-25 10:27:42 +0300

    first commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyOverlayApp extends StatelessWidget {
+  const MyOverlayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Hi'),),
+    );
+  }
+}
